60060c4daf1f42171644ac580750a11a8e5ff7ed
marcelopedroso20 <68392028+marcelopedroso20@users.noreply.github.com>
2025-10-10 13:52:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 import express from "express";
 import cors from "cors";
 import db from "./db.js";
+import efetivoRoutes from "./routes/efetivo.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 // ⬇️ Aqui você adiciona a linha da rota nova
-import efetivoRoutes from "./routes/efetivo.js";
 app.use("/api/efetivo", efetivoRoutes);
 
 // teste
 app.get("/", (req, res) => {
-  res.send("API ROTAM ativa!");
+  res.send("API ROTAM funcionando!");
 });
 
 const PORT = process.env.PORT || 3000;
