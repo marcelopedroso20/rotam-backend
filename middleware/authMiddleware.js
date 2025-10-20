8d67985c8c28c52e47f467c0491e727f7c77bf4d
marcelopedroso20 <68392028+marcelopedroso20@users.noreply.github.com>
2025-10-20 17:32:07 -0400

    Add files via upload

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,17 @@
 // middleware/authMiddleware.js
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
 const JWT_SECRET = process.env.JWT_SECRET || "segredo_super_forte";
 
 export function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "Token não fornecido." });
+  if (!token) return res.status(401).json({ error: "Token ausente" });
+
   jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: "Token inválido ou expirado." });
+    if (err) return res.status(403).json({ error: "Token inválido" });
     req.user = user;
     next();
   });
