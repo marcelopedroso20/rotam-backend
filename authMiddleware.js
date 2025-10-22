b3a88bfdab1ff6cac9615808301e201113b91348
marcelopedroso20 <68392028+marcelopedroso20@users.noreply.github.com>
2025-10-22 14:02:53 -0400

    Add files via upload

---
+++ authMiddleware.js
@@ -0,0 +1,14 @@
+// middleware/authMiddleware.js
+import jwt from "jsonwebtoken";
+const JWT_SECRET = process.env.JWT_SECRET || "segredo_super_forte";
+
+export function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "Token não fornecido." });
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: "Token inválido ou expirado." });
+    req.user = user;
+    next();
+  });
+}
